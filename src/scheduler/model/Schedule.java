b36e6aabbc907425794e6e3ecee3f36d3411482f
SBe <szymonbezpalko@gmail.com>
2017-01-07 00:36:15 +0100

    Update Schedule.java

--- src/scheduler/model/Schedule.java
+++ src/scheduler/model/Schedule.java
@@ -15,10 +15,17 @@ public class Schedule {
     private Lesson[][][] schedule;
     private boolean[][][] isUsed;
     private double fitness;
-    
+    private int mutationSize;
+    private int crossoverSize;
+    private int mutationProbability;
+    private int crossoverProbability;
     public Schedule(){
         this.schedule = new Lesson[Data.getDays()][Data.getHours()][Data.getGroupCount()];
         this.isUsed = new boolean[Data.getDays()][Data.getHours()][Data.getGroupCount()];
+        this.mutationSize = 5000;
+        this.crossoverSize = 3;
+        this.mutationProbability = 99;
+        this.crossoverProbability = 20;
     }
     
     public void generateSchedule(){
@@ -89,19 +96,54 @@ public class Schedule {
     public double getFitness(){
         return fitness;
     }
-    
-    public void crossover(){
         
-    }
-    
     public void mutation(){
-        
+    	Random rand = new Random();   
+    	if(rand.nextInt(100) < this.mutationProbability){
+	        for(int i = 0; i < this.mutationSize; i++){
+	        	int a = rand.nextInt(Data.getDays());
+	        	int b = rand.nextInt(Data.getHours());
+	        	int c = rand.nextInt(Data.getDays());
+	        	int d = rand.nextInt(Data.getHours());
+	        	int k = rand.nextInt(Data.getGroupCount());
+	        	if(isUsed[a][b][k] == true || isUsed[c][d][k] == true){
+		        	Lesson tmp = schedule[a][b][k];
+		        	schedule[a][b][k] = schedule[c][d][k];
+		        	schedule[c][d][k] = tmp;
+		        	if(schedule[c][d][k] != null){
+		        		mutation(schedule[c][d][k], rand);
+		        		isUsed[c][d][k] = true;
+		        	}
+		        	else
+		        		isUsed[c][d][k] = false;
+		        	if(schedule[a][b][k] != null){
+		        		mutation(schedule[a][b][k], rand);
+		        		isUsed[a][b][k] = true;
+		        	}
+		        	else
+		        		isUsed[a][b][k] = false;
+	        	}   
+	        }
+    	}
     }
-    
+    public void mutation(Lesson l, Random rand){
+    	do{
+			Room myRoom = Data.getRooms().get(rand.nextInt(Data.getRooms().size()));
+			if(myRoom.getSize().get() > l.getGroup().getSize().get()){
+				l.setRoom(myRoom);        		
+				break;
+			}
+		} while(true);
+    }
     public Lesson[][][] getSchedule(){
         return schedule;
     }
-    
+    public int getCrossoverSize(){
+    	return this.crossoverSize;
+    }
+    public void setSchedule(int day, int hour, int group, Lesson l){
+    	this.schedule[day][hour][group] = l;
+    }
     public boolean[][][] getIsUsed(){
         return isUsed;
     }
